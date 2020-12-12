5f5938f14bbe298cc32f2e78fe3a09a72ffd2323
Greg Mall <gregmall157@gmail.com>
2020-12-12 14:54:38 -0800

    more

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import styles from './App.css';
 
 export default function App() {
-  return <h1>This is Greg's Portfolio Site.  WELCOME!!! HOPE YOU LIKE HUGE TEXT!</h1>;
+  return (
+    <div className={styles.main}>
+  <h1>This is Greg's Portfolio Site.  WELCOME!!! HOPE YOU LIKE HUGE TEXT!</h1>;
+  </div>
+  )
 }
