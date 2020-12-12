a448790591a1e2c5143c7e7cbcb8150161c23b97
Greg Mall <gregmall157@gmail.com>
2020-12-12 12:36:26 -0800

    added welcome

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './App.css';
 
 export default function App() {
-  return <h1>Hello Greg's Portfolio</h1>;
+  return <h1>This is Greg's Portfolio Site.  WELCOME!!! HOPE YOU LIKE HUGE TEXT!</h1>;
 }
