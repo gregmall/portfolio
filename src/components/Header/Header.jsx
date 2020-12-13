0979e711eee99a58a47dc54090c05e01406ed334
Greg Mall <gregmall157@gmail.com>
2020-12-12 16:11:41 -0800

    working on it

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () =>{
+  return (
+    <header>
+    <div>
+      Greg's Portfolio
+    </div>
+    </header>
+  )
+}
+export default Header;
+
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
-    <div className={styles.main}>
+    <Router>
+      <Header />
+    <div className={styles.app}>
   <h1>This is Greg's Portfolio Site.  WELCOME!!! HOPE YOU LIKE HUGE TEXT!</h1>;
   </div>
+  </Router>
   )
 }
