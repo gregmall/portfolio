9f6b9cc8db26e20bcbf876f0d0ac77e0f28f3d80
Greg Mall <gregmall157@gmail.com>
2021-01-10 17:58:54 -0800

    tooltips

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,12 +4,12 @@ export default function Contact() {
   return (
     <div className = {styles.Contact}>
       <div className = {styles.grow}>
-        <div className={styles.links}>
-      <div ><a href="https://github.com/gregmall" target="blank"><img src="https://visualpharm.com/assets/624/Github-595b40b85ba036ed117dc155.svg" /><span>CHECK MY GITHUB</span></a></div>
-      <div ><a href="https://www.linkedin.com/in/greg-mall-3032771b1/" target="blank"><img src="https://www.flaticon.com/svg/static/icons/svg/61/61109.svg" /><span>HIT UP MY LINKEDIN</span></a></div>
-      <div ><a href="https://twitter.com/GregMall3" target="blank"><img src="https://www.flaticon.com/svg/static/icons/svg/60/60580.svg" /><span>FOLLOW ME ON TWITTER</span></a></div>
-      <div ><a href="mailto:gregmall157@gmail.com"><img src="https://www.flaticon.com/svg/static/icons/svg/115/115757.svg" /><span>EMAIL ME! LETS CHAT!!</span></a></div>
-      </div>
+      
+      <div className={styles.box}><a href="https://github.com/gregmall" target="blank"><img src="https://visualpharm.com/assets/624/Github-595b40b85ba036ed117dc155.svg" /><span>GITHUB</span></a></div>
+      <div className={styles.box}><a href="https://www.linkedin.com/in/greg-mall-3032771b1/" target="blank"><img src="https://www.flaticon.com/svg/static/icons/svg/61/61109.svg" /><span>LINKEDIN</span></a></div>
+      <div className={styles.box}><a href="https://twitter.com/GregMall3" target="blank"><img src="https://www.flaticon.com/svg/static/icons/svg/60/60580.svg" /><span>TWITTER</span></a></div>
+      <div className={styles.box}><a href="mailto:gregmall157@gmail.com"><img src="https://www.flaticon.com/svg/static/icons/svg/115/115757.svg" /><span>EMAIL</span></a></div>
+      
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import './index.css';
+
 
 render(
   
