84f2e8c75a4302bdb374f5e60078c0e156b01d1e
Greg Mall <gregmall157@gmail.com>
2021-01-19 10:34:10 -0800

    edit home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <img src="https://avatars3.githubusercontent.com/u/61127092?s=460&u=15e04a694526d54571dbcb76a57bcb0636b52ea4&v=4"/>
       </div>
       <div className={styles.main}>
-      <h1>Full-stack software engineer with a passion for creating and technology. A lifelong musician and a professional artist, Greg brings an artist's eye to solutions and a musicians ability to collaborate on projects.</h1>
+      <h1>Hello! I'm Greg Mall, a full-stack software engineer with a passion for creating and technology. A lifelong musician and a professional artist, I bring an artist's eye to solutions and a musician's ability to collaborate on projects.</h1>
       </div>
     </div>
   )
