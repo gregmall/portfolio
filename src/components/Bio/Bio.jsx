f733456554e5aacfb30c3485d18dec82c86b9601
Greg Mall <gregmall157@gmail.com>
2021-02-04 16:30:19 -0800

    added slider feature

--- src/components/Bio/Bio.jsx
+++ src/components/Bio/Bio.jsx
@@ -1,7 +1,47 @@
 import React from 'react'
 import Iframe from 'react-iframe'
 import styles from './Bio.css';
+import AwesomeSlider from 'react-awesome-slider';
+import builtInSliderStyles from 'react-awesome-slider/dist/styles.css'
+
+
 export default function Bio() {
+
+  const bandSlider = (
+    <AwesomeSlider cssModule={builtInSliderStyles} className={styles.slider}>
+      <div className={styles.cede}>
+        <h3>Cede the Clouds</h3>
+        <img src="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.0-9/48368437_2218654741585377_2919862245498290176_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=sYsOgRmawK8AX-IETVk&_nc_ht=scontent.fhio2-2.fna&oh=681768f92c7ae30b85ebd6560dd7d8c7&oe=60271CC2" />
+        <Iframe url="https://bandcamp.com/EmbeddedPlayer/album=2964969569/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2130197304/transparent=true/" 
+        width="400px"
+        height="120px"
+        id="myId"
+        
+        display="initial"
+        />
+      </div>
+      <div className={styles.pigs}>
+        <h3>Feral Pigs</h3>
+        <img src="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.0-9/430573_367092509968069_1524082485_n.jpg?_nc_cat=104&ccb=2&_nc_sid=cdbe9c&_nc_ohc=rZVLShOFhJ0AX_74K9D&_nc_ht=scontent.fhio2-2.fna&oh=d98566eebfb146bef41628f56b63318e&oe=602515EE"/>
+        <Iframe url="https://www.reverbnation.com/widget_code/html_widget/artist_2390217?widget_id=55&pwc[song_ids]=17216491&context_type=song&spoid=artist_2390217"
+        width="400px"
+        height="120px"
+        id="myId"
+        
+        display="initial"/>
+      </div>
+      <div className={styles.secondafter}>
+        <h3>Secondafter</h3>
+        <img src="https://scontent.fhio2-1.fna.fbcdn.net/v/t1.0-9/5200_117486697215_581316_n.jpg?_nc_cat=111&ccb=2&_nc_sid=cdbe9c&_nc_ohc=UgyrHA6exJoAX-SRhrf&_nc_ht=scontent.fhio2-1.fna&oh=cbf812bcf68756e587a207de53fa6c2a&oe=6025103A"/>
+        
+        <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/614894913&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+          height="100px"
+        id="myId"
+        
+        display="initial"/>
+      </div>
+    </AwesomeSlider>
+  )
   return (
     <>
       <div className={styles.title}>
@@ -19,39 +59,11 @@ export default function Bio() {
         <div className={styles.description}>
         <p>Music has been a lifelong passion of mine.  I've been playing a musical instrument of some kind since i was 8 years old.  I've always been drawn the the creativity of sound.  I joined my first band in highschool and have been playing in one band or another ever since.  </p>
         </div>
-        <div className={styles.cede}>
-        <h3>Cede the Clouds</h3>
-        <img src="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.0-9/48368437_2218654741585377_2919862245498290176_o.jpg?_nc_cat=110&ccb=2&_nc_sid=09cbfe&_nc_ohc=sYsOgRmawK8AX-IETVk&_nc_ht=scontent.fhio2-2.fna&oh=681768f92c7ae30b85ebd6560dd7d8c7&oe=60271CC2" />
-   <Iframe url="https://bandcamp.com/EmbeddedPlayer/album=2964969569/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/track=2130197304/transparent=true/" 
-   width="400px"
-   height="120px"
-   id="myId"
-   
-   display="initial"
-   />
+        
+     <div className={styles.slider}>{bandSlider}</div>
+        
    </div>
    
-<div className={styles.pigs}>
-   <h3>Feral Pigs</h3>
-   <img src="https://scontent.fhio2-2.fna.fbcdn.net/v/t1.0-9/430573_367092509968069_1524082485_n.jpg?_nc_cat=104&ccb=2&_nc_sid=cdbe9c&_nc_ohc=rZVLShOFhJ0AX_74K9D&_nc_ht=scontent.fhio2-2.fna&oh=d98566eebfb146bef41628f56b63318e&oe=602515EE"/>
-   <Iframe url="https://www.reverbnation.com/widget_code/html_widget/artist_2390217?widget_id=55&pwc[song_ids]=17216491&context_type=song&spoid=artist_2390217"
-   width="400px"
-   height="120px"
-   id="myId"
-   
-   display="initial"/>
- </div>
- <div className={styles.secondafter}>
-   <h3>Secondafter</h3>
-   <img src="https://scontent.fhio2-1.fna.fbcdn.net/v/t1.0-9/5200_117486697215_581316_n.jpg?_nc_cat=111&ccb=2&_nc_sid=cdbe9c&_nc_ohc=UgyrHA6exJoAX-SRhrf&_nc_ht=scontent.fhio2-1.fna&oh=cbf812bcf68756e587a207de53fa6c2a&oe=6025103A"/>
-   
-   <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/614894913&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-    height="100px"
-   id="myId"
-   
-   display="initial"/>
- </div>
-   </div>
    <h1>...art</h1>
    <div className={styles.art}>
      
