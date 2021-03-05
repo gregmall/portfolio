d2891333834c3a054166838fae10be6d93c56e8f
Greg Mall <gregmall157@gmail.com>
2021-03-04 16:04:07 -0800

    put pictures in local file

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
           <a href="https://github.com/VTMFOA/VTMFOA" target="blank">GITHUB</a>
          
         </nav>
-        <img src="https://pbs.twimg.com/media/Ej6oSZdXsAIsN56?format=jpg&name=large" />
+        <img src="/assets/vobot.jpeg" />
         <h4>Techs used: Node, PostgreSQL, Jest, Heroku, Twilio, Twitter</h4>
         <p>A Twitter bot that sends state specific voting information to users via text message or via Tweet. Using the Twit library from the Twitter API I implemented the bot function that replied to Twitter users who tweeted at our bot with a state name. I also created the database of quotes that our bot tweeted using PostgreSQL.</p>
       </div>
@@ -24,7 +24,7 @@ export default function Projects() {
         <a href="https://whats-cookn.netlify.app/" target="blank">SITE </a>
         <a href="https://github.com/Aluminum-Chefs" target="blank">GITHUB</a>
         </nav>
-        <img src="https://pbs.twimg.com/media/EsdwtDPU4AAmg3R?format=jpg&name=large" />
+        <img src="/assets/cookn.jpeg" />
         <h4>Techs used:  React, Node, Jest, PosrtgreSQL, React Sidenav, Calendar IO</h4>
         <p>A recipe search and meal planning app that lets the user schedule meals or a week or a month. I worked with a teammate to build the back end of our app using Node, creating the database and endpoints that would serve the front end. I also implemented the calendar using Calendar IO for React.</p>
       </div>
@@ -34,7 +34,7 @@ export default function Projects() {
           <a href="https://thecanary.netlify.app/" target="blank">SITE </a>
           <a href="https://github.com/The-Canary-Project" target="blank">GITHUB</a>
         </nav>
-        <img src="https://pbs.twimg.com/media/EsdxN1fVkAAK2gm?format=jpg&name=large" />
+        <img src="/assets/canary.jpeg" />
         <h4>Techs used:  React, Node, CSS, TensorFlow, Socket.IO</h4>
         <p>Canary is a learning platform that makes virtual learning environments that engage students through movement. This app was built during a week long sprint. We utilized socket.io technology to create a virtual classroom and TensorFlow pose recognition to detect gestures as responses. I was tasked with implementing the calibration page of the app that allowed users to determine what gestures they would use as responses</p>
       </div>
