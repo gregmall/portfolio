a6199bab1e320e76badf9890dd38135bced63bfa
Greg Mall <gregmall157@gmail.com>
2021-05-28 11:10:52 -0700

    fixed error for netlify

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -16,32 +16,32 @@ export default function Tech() {
         <div className={styles.tooltip}>
       
         
-        <img src="public/techs/jslogo.webp"/>
+        <img src="/techs/jslogo.webp"/>
        
         <caption>JavaScript</caption>
         
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/htmllogo.png" />
+        <img src="/techs/htmllogo.png" />
         <caption>HTML5</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/csslogo.webp" />
+        <img src="/techs/csslogo.webp" />
         <caption>CSS3</caption>
         </div>
       </div>
       <div className={styles.box}>
         <h2>Frameworks</h2>
         <div className={styles.tooltip}>
-        <img src="public/techs/reactlogo.png"/>
+        <img src="/techs/reactlogo.png"/>
         <caption>ReactJS</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/nodelogo.webp" />
+        <img src="/techs/nodelogo.webp" />
         <caption>NodeJS</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/kisspng-redux-react-javascript-vue-js-single-page-applicat-5af5cde424ee15.8061979615260584681513.png" />
+        <img src="/techs/kisspng-redux-react-javascript-vue-js-single-page-applicat-5af5cde424ee15.8061979615260584681513.png" />
         <caption>Redux</caption>
         </div>
 
@@ -50,40 +50,45 @@ export default function Tech() {
      
         <h2>Dev Tools</h2>
         <div className={styles.tooltip}>
-        <img src="public/techs/githublogo.png" />
+        <img src="/techs/githublogo.png" />
         <caption>Github</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/herokulogo.webp" />
+        <img src="/techs/herokulogo.webp" />
         <caption>Heroku</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/netlifylogo.png"/>
+        <img src="/techs/netlifylogo.png"/>
         <caption>Netlify</caption>
         </div>
       </div>
       <div className={styles.box}>
         <h2>Testing Suites</h2>
         <div className={styles.tooltip}>
-        <img src ="public/techs/jestlogo.webp"  />
+        <img src ="/techs/jestlogo.webp"  />
         <caption>Jest</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/quinitlogo.com.png" />
+        <img src="/techs/quinitlogo.com.png" />
         <caption>QUnit</caption>
         </div>
       </div>
       <div className={styles.box}>
         <h2>Database</h2>
         <div className={styles.tooltip}>
-        <img src="public/techs/postgresqllogo.webp" />
+        <img src="/techs/postgresqllogo.webp" />
         <caption>PostgreSQL</caption>
         </div>
         <div className={styles.tooltip}>
-        <img src="public/techs/mysqllogo.webp"/>
+        <img src="/techs/mysqllogo.webp"/>
         <caption>MySQL</caption>
         </div>
+        <div className={styles.tooltip}>
+        <img src="/techs/kisspng-firebase-cloud-messaging-computer-icons-google-clo-github-5ad5d3ce4fc460.4730334415239628303267.png"/>
+        <caption>Firebase</caption>
+        </div>
       </div>
+      
       
     </div>
     </div>
